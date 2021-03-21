f7a75d0d6ec786dd30498450c31f49e10b1e04d7
juliomenaya <juliomenaya@gmail.com>
2021-03-21 15:55:00 -0600

    search for a student and controll if it does not exist

--- src/resolvers/query.ts
+++ src/resolvers/query.ts
@@ -1,7 +1,8 @@
 import { IResolvers } from 'graphql-tools';
 import { database } from '../data/data.store';
+import _ from 'lodash';
 
-interface Student {
+interface IStudent {
     id: string,
     name: string,
     email: string,
@@ -12,8 +13,20 @@ interface Student {
 
 const query: IResolvers = {
     Query: {
-        students(): Student[] {
+        students(): IStudent[] {
             return database.students;
+        },
+        student(__: void, { id } ): IStudent {
+            const dbStudent = _.find(database.students, ['id', id]);
+            if (dbStudent === undefined) {
+                return {
+                    id: '-1',
+                    name: `Could not find student with ID ${id}`,
+                    email: '',
+                    courses: ['']
+                } as IStudent
+            }
+            return dbStudent
         }
     }
 };
