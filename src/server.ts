a8a05b49b73d8f7b6f719e228926384c5d375262
juliomenaya <juliomenaya@gmail.com>
2021-03-21 12:33:57 -0600

    express playground working

--- src/server.ts
+++ src/server.ts
@@ -2,15 +2,25 @@ import express from 'express';
 import compression from 'compression';
 import { createServer } from 'http';
 import cors from 'cors';
+import expressPlayGround from 'graphql-playground-middleware-express';
+import { ApolloServer } from 'apollo-server-express';
+import schema from './schema';
 
 const app = express();
 
 app.use(cors());
 app.use(compression());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to online academy API')
+const apolloServer = new ApolloServer({
+    schema,
+    introspection: true
 });
+
+apolloServer.applyMiddleware( { app });
+
+app.get('/', expressPlayGround({
+    endpoint: '/graphql'
+}));
 
 const httpServer = createServer(app);
 const PORT = 5200;
@@ -19,6 +29,6 @@ httpServer.listen(
     {
         port: PORT
     },
-    () => console.log(`Server running at: http://localhost:${PORT}`)
+    () => console.log(`Server running at: http://localhost:${PORT}/graphql`)
 );
 
