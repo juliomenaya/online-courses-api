e448e5fc274f8ded8caa45646595947165875888
juliomenaya <juliomenaya@gmail.com>
2021-03-21 17:06:31 -0600

    accessing students info from course object

--- src/resolvers/type.ts
+++ src/resolvers/type.ts
@@ -8,6 +8,12 @@ const type: IResolvers = {
         courses: parent => {
             return _.filter(database.courses, dbCourse => parent.courses.includes(dbCourse.id));
         }
+    },
+    Course: {
+        students: parent => {
+            return _.filter(database.students, student => student.courses.includes(parent.id));
+        },
+        path: parent => `https://www.udemy.com${parent.path}` 
     }
 };
 
